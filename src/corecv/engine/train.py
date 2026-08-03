d1105ce4c6e63cd466d859f62c9d0ba6bdd46a54
Lucas Iturriago <70216214+liturriago@users.noreply.github.com>
2026-08-02 22:49:11 -0500

    Log metric results after each epoch in Trainer

--- src/corecv/engine/train.py
+++ src/corecv/engine/train.py
@@ -234,4 +234,7 @@ class Trainer:
                 val_metrics["loss"],
             )
 
+            self._train_metrics.print_results()
+            self._val_metrics.print_results()
+
         return dict(history)
